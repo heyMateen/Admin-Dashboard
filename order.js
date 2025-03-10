6a6740f4cd057b290e207074680e8848f20620eb
Mateen Haider <120988104+heyMateen@users.noreply.github.com>
2025-03-11 00:57:05 +0500

    structure designed

---
+++ order.js
@@ -0,0 +1,20 @@
+const orders = [
+  {
+    productName: "Javascript Tutorial",
+    productNumber: "85743",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "CSS Full Course",
+    productNumber: "97653",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Flex-Box Tutorial",
+    productNumber: "45689",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
